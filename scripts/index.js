00148e990082d524f733114bd3098d3222fb5170
Christian Lin <christianlinlin123@gmail.com>
2024-06-26 16:17:11 -0400

    added scroll animations

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-    //IntersectionObserver for the topImages
+document.addEventListener("DOMContentLoaded", function() {
+    // IntersectionObserver for the topImages
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            console.log('Entry:', entry);
             if (entry.isIntersecting) {
                 entry.target.classList.add('translateDown');
             } else {
@@ -11,26 +11,43 @@
     }, {
         root: null,
         rootMargin: '0px',
-        threshold: 0.5
+        threshold: 0.2
     });
 
     const topImages = document.querySelectorAll('.top');
     topImages.forEach((el) => observer.observe(el));
 
-    //IntersectionObserver for the bottomImages
-const secondObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log('Entry:', entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('translateUp');
-        } else {
-            entry.target.classList.remove('translateUp');
-        }
+    // IntersectionObserver for the bottomImages
+    const secondObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('translateUp');
+            } else {
+                entry.target.classList.remove('translateUp');
+            }
+        });
+    }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
     });
-}, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.3
-});
+
     const bottomImages = document.querySelectorAll('.bottom');
     bottomImages.forEach((el) => secondObserver.observe(el));
+
+        // IntersectionObserver for the skillsAreaText
+        const thirdObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                console.log('Entry:', entry); // Add this line for debugging
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('translateUp1');
+                }
+            });
+        }, {
+            root: null,
+            threshold: 0.1
+        });
+    
+        const skillsAreaText = document.querySelectorAll('.skills-text');
+        skillsAreaText.forEach((el) => thirdObserver.observe(el));
+});
