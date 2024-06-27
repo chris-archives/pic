cdc81f2a6c404db077f5ed3f893987aa6924f177
Christian Lin <christianlinlin123@gmail.com>
2024-06-26 23:18:57 -0400

    fixing bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // IntersectionObserver for the topImages
-    const observer = new IntersectionObserver((entries) => {
+    const topObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('translateDown');
@@ -15,10 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     const topImages = document.querySelectorAll('.top');
-    topImages.forEach((el) => observer.observe(el));
+    topImages.forEach((el) => topObserver.observe(el));
 
     // IntersectionObserver for the bottomImages
-    const secondObserver = new IntersectionObserver((entries) => {
+    const bottomObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('translateUp');
@@ -33,10 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     const bottomImages = document.querySelectorAll('.bottom');
-    bottomImages.forEach((el) => secondObserver.observe(el));
+    bottomImages.forEach((el) => bottomObserver.observe(el));
 
     // IntersectionObserver for the skillsAreaText
-    const thirdObserver = new IntersectionObserver((entries) => {
+    const skillsObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 const elements = entry.target.querySelectorAll('.skills-text');
@@ -57,6 +57,26 @@ document.addEventListener("DOMContentLoaded", function() {
         threshold: 0.1
     });
 
-    const skillsSection = document.querySelectorAll('#skills-section');
-    skillsSection.forEach((el) => thirdObserver.observe(el));
+    const skillsSections = document.querySelectorAll('#skills-section');
+    skillsSections.forEach((el) => skillsObserver.observe(el));
+
+    // IntersectionObserver for Skill Tree
+    const bannerObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('lazy-load');
+            } else {
+                entry.target.classList.remove('lazy-load');
+            }
+        });
+    }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    });
+
+    const banners = document.querySelectorAll('.banner');
+    banners.forEach((el) => bannerObserver.observe(el));
+
+    // Another IntersectionObserver for overlay/banner visibility
 });
