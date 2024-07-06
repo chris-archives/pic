c80b789b1a8f7c72dc17242841c25171e96cd785
Christian Lin <christianlinlin123@gmail.com>
2024-07-06 12:39:48 -0400

    finished interactive roadmap

--- scripts/courses.js
+++ scripts/courses.js
@@ -52,4 +52,52 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const mapPins = document.querySelectorAll('#roadmap .svg-container img');
     mapPins.forEach((el) => mapPinObserver.observe(el));
+
+    //Hover effect for Map pins (#innovate-text)
+    document.querySelector('#pin-one').addEventListener("mouseenter", () => {
+        document.querySelector("#innovate-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-one').addEventListener("mouseleave", () => {
+        document.querySelector("#innovate-text").style.opacity = "0";
+    });
+
+    //Hover effect for Map pins (#wireframe-text)
+    document.querySelector('#pin-two').addEventListener("mouseenter", () => {
+        document.querySelector("#wireframe-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-two').addEventListener("mouseleave", () => {
+        document.querySelector("#wireframe-text").style.opacity = "0";
+    });
+
+    //Hover effect for Map pins (#design-text)
+    document.querySelector('#pin-three').addEventListener("mouseenter", () => {
+        document.querySelector("#design-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-three').addEventListener("mouseleave", () => {
+        document.querySelector("#design-text").style.opacity = "0";
+    });
+
+    //Hover effect for Map pins (#code-text)
+    document.querySelector('#pin-four').addEventListener("mouseenter", () => {
+        document.querySelector("#code-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-four').addEventListener("mouseleave", () => {
+        document.querySelector("#code-text").style.opacity = "0";
+    });
+
+    //Hover effect for Map pins (#debug-text)
+    document.querySelector('#pin-five').addEventListener("mouseenter", () => {
+        document.querySelector("#debug-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-five').addEventListener("mouseleave", () => {
+        document.querySelector("#debug-text").style.opacity = "0";
+    });
+
+    //Hover effect for Map pins (#publish-text)
+    document.querySelector('#pin-six').addEventListener("mouseenter", () => {
+        document.querySelector("#publish-text").style.opacity = "1";
+    });
+    document.querySelector('#pin-six').addEventListener("mouseleave", () => {
+        document.querySelector("#publish-text").style.opacity = "0";
+    });
 });
