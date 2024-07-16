97f75f2607db398ca3ed6babb7ac56cacf51787e
Christian Lin <christianlinlin123@gmail.com>
2024-07-16 17:47:02 -0400

    add javascript to role cards

---
+++ scripts/about.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //IntersectionObserver for Main
+    const mainPageObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            } else {
+                entry.target.classList.remove('animate');
+            }
+            });
+    }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    });
+
+    const mainPage = document.querySelectorAll('#main-section');
+    mainPage.forEach((el) => mainPageObserver.observe(el));
+
+    //IntersectionObserver for Roles text
+    const rolesTextObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            } else {
+                entry.target.classList.remove('animate');
+            }
+        });
+    }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    });
+
+    const rolesText = document.querySelectorAll('#animation-div');
+    rolesText.forEach((el) => rolesTextObserver.observe(el));
+
+      // IntersectionObserver for the Role Cards
+        const roleCardsObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const elements = entry.target.querySelectorAll('.cards');
+                    elements.forEach((el, index) => {
+                        el.style.transitionDelay = `${index * 0.2}s`;
+                        el.classList.add('animate');
+                    });
+                } else {
+                    const elements = entry.target.querySelectorAll('.cards');
+                    elements.forEach((el) => {
+                        el.classList.remove('animate');
+                        el.style.transitionDelay = '0s';
+                    });
+                }
+            });
+        }, {
+            root: null,
+            threshold: 0.1
+        });
+
+    const roleCards = document.querySelectorAll('#cards-container');
+    roleCards.forEach((el) => roleCardsObserver.observe(el));
+});
